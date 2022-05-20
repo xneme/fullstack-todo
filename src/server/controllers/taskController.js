39b771f82953fe748ce4dd92dd671135395fa4df
Jere Helén <xneme@me.com>
2022-05-20 22:00:57 +0100

    Add tasks CRUD to backend

--- src/server/controllers/taskController.js
+++ src/server/controllers/taskController.js
@@ -11,4 +11,37 @@ const getTasks = async (req, res) => {
   }
 }
 
-module.exports = {getTasks}
+const addTask = async (req, res) => {
+  try {
+    const task = req.body
+    const newTask = await db.tasks.create(task)
+    res.status(200).json(newTask)
+  } catch (e) {
+    logger.error(e.message)
+    res.status(500).json({ error: 'server went BOOM!' })
+  }
+}
+
+const editTask = async (req, res) => {
+  try {
+    const task = req.body
+
+    const [rows, [updatedTask]] = await db.tasks.update(task, {
+      returning: true,
+      where: { id: req.params.id }
+    })
+
+    if (rows) return res.status(200).json(updatedTask)
+    return res.status(400).json({ error: 'id not found.' })
+  } catch (e) {
+    logger.error(e.message)
+    res.status(500).json({ error: 'server went BOOM!' })
+  }
+}
+
+const deleteTask = async (req, res) => {
+  await db.tasks.destroy({ where: { id: req.params.id } })
+  return res.status(200).json({ id: req.params.id })
+}
+
+module.exports = {getTasks, addTask, editTask, deleteTask}
--- src/server/utils/routes.js
+++ src/server/utils/routes.js
@@ -1,9 +1,12 @@
 const Router = require('express')
 
-const { getTasks } = require('../controllers/taskController')
+const { getTasks, addTask, editTask, deleteTask } = require('../controllers/taskController')
 
 const router = Router()
 
 router.get('/tasks', getTasks)
+router.post('/tasks', addTask)
+router.put('/tasks/:id', editTask)
+router.delete('/tasks/:id', deleteTask)
 
 module.exports = router
