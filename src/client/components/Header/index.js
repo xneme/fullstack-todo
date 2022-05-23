a630b132eaa1597f30670af9e26ef5df9e30d84d
Jere Helén <xneme@me.com>
2022-05-23 11:34:44 +0100

    Add link to github

--- src/client/components/Header/index.js
+++ src/client/components/Header/index.js
@@ -3,6 +3,8 @@ import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
+import Link from '@mui/material/Link'
+import Button from '@mui/material/Button'
 
 export default () => {
   return (
@@ -12,6 +14,9 @@ export default () => {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Yet another fullstack TODO app
           </Typography>
+          <Link href="https://github.com/xneme/fullstack-todo">
+            <Button color="inherit">Project in Github</Button>
+          </Link>
         </Toolbar>
       </AppBar>
     </Box>
