c5642c029ab8e2e71f080e0b5a346c0260913a30
Jere Helén <xneme@me.com>
2022-05-25 20:50:09 +0100

    Sort completed cards to bottom

--- src/client/components/Tasks/TaskList.js
+++ src/client/components/Tasks/TaskList.js
@@ -6,9 +6,11 @@ import Task from './Task'
 export default () => {  
   const { data, error, isLoading } = useGetTasksQuery()
   if (isLoading) return null
+  
+  // Make a copy of the tasks array, sort it and create components
   return (
     <>
-      {data.map((task) => (
+      {[...data].sort((a) => a.completed ? 1 : -1).map((task) => (
           <Task task={task} key={task.id} />
       ))}
     </>
