3c939b9335df9d3036a5695edff474a937cba5d9
Jere Helén <xneme@me.com>
2022-05-25 21:13:15 +0100

    Sort tasks by creation date

--- src/client/components/Tasks/TaskList.js
+++ src/client/components/Tasks/TaskList.js
@@ -2,15 +2,22 @@ import React from 'react'
 import { useGetTasksQuery } from './taskSlice'
 import Task from './Task'
 
+const sortByCompleted = (a, b) => {
+  if (a.completed && b.completed) return 0
+  if (a.completed) return 1
+  return -1
+}
 
 export default () => {  
   const { data, error, isLoading } = useGetTasksQuery()
   if (isLoading) return null
-  
-  // Make a copy of the tasks array, sort it and create components
+  console.log(data)
   return (
     <>
-      {[...data].sort((a) => a.completed ? 1 : -1).map((task) => (
+      {[...data]                                                      // Make copy of the array
+        .sort((a,b) => new Date(a.createdAt) - new Date(b.createdAt)) // Sort by creation data
+        .sort(sortByCompleted)                                        // Sort by completed
+        .map((task) => (
           <Task task={task} key={task.id} />
       ))}
     </>
